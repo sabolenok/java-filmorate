7d585d1d7275ace219772219417ec196496313ad
sabolenok <d5sp2004@yandex.ru>
2022-09-06 21:59:24 +0300

    Логирование для эндпойнтов POST и PUT-запросов в FilmController и UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.controller;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.ValidationException;
 import java.time.LocalDate;
@@ -10,7 +11,10 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class FilmController {
 
     private final Map<Integer, Film> films = new HashMap<>();
@@ -21,10 +25,12 @@ public class FilmController {
     }
 
     @RequestMapping(value = "/films", method = {RequestMethod.POST, RequestMethod.PUT})
-    public Film create(@Valid @RequestBody Film film) {
+    public Film create(@Valid @RequestBody Film film, HttpServletRequest request) {
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
             throw new ValidationException("Дата релиза должна быть не раньше 28.12.1895");
         }
+        log.info("Получен запрос к эндпоинту: '{} {}', Строка параметров запроса: '{}'",
+                request.getMethod(), request.getRequestURI(), request.getQueryString());
         films.put(film.getId(), film);
         return film;
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,14 +3,18 @@ package ru.yandex.practicum.filmorate.controller;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.ValidationException;
-import java.time.LocalDate;
+
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class UserController {
 
     private final Map<Integer, User> users = new HashMap<>();
@@ -21,13 +25,15 @@ public class UserController {
     }
 
     @RequestMapping(value = "/users", method = {RequestMethod.POST, RequestMethod.PUT})
-    public User create(@Valid @RequestBody User user) {
+    public User create(@Valid @RequestBody User user, HttpServletRequest request) {
         if (user.getLogin().contains(" ")) {
             throw new ValidationException("Логин не может содержать пробелы");
         }
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
         }
+        log.info("Получен запрос к эндпоинту: '{} {}', Строка параметров запроса: '{}'",
+                request.getMethod(), request.getRequestURI(), request.getQueryString());
         users.put(user.getId(), user);
         return user;
     }
