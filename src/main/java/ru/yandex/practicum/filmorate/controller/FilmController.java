afe235306d1d37a6087c5d158c116b7882659dc8
sabolenok <d5sp2004@yandex.ru>
2022-09-06 21:05:59 +0300

    эндпойнты для RestController'ов

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,25 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class FilmController {
+
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    @GetMapping("/films")
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @RequestMapping(value = "/films", method = {RequestMethod.POST, RequestMethod.PUT})
+    public Film create(@RequestBody Film film) {
+        films.put(film.getId(), film);
+        return film;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,7 +1,25 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
+
+    private final Map<Integer, User> users = new HashMap<>();
+
+    @GetMapping("/users")
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @RequestMapping(value = "/users", method = {RequestMethod.POST, RequestMethod.PUT})
+    public User create(@RequestBody User user) {
+        users.put(user.getId(), user);
+        return user;
+    }
 }
