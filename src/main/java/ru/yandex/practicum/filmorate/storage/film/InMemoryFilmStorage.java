ed85e13836166a7bcb1e58a58d179b04470cede6
sabolenok <d5sp2004@yandex.ru>
2022-09-15 20:26:54 +0300

    в класс InMemoryFilmStorage перенесены методы из контроллера

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -1,4 +1,38 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class InMemoryFilmStorage implements FilmStorage {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private static Integer id;
+
+    private static Integer getNextId() {
+        return id++;
+    }
+
+    @Override
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @Override
+    public Film create(Film film) {
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public Film put(Film film) {
+        if (!films.containsKey(film.getId())) {
+            throw new NotFoundException("Фильм не найден");
+        }
+        films.put(film.getId(), film);
+        return film;
+    }
 }
