fd2154bd75304223be3546c1358a5338c5e35b47
sabolenok <d5sp2004@yandex.ru>
2022-10-20 21:20:23 +0530

    в классе UserDbStorage реализовано добавление друзей

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -4,15 +4,21 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Set;
 
 @Component("inDbUser")
 @Slf4j
@@ -46,18 +52,27 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public User create(User user) {
-        jdbcTemplate.update(
-                "insert into users (email, login, user_name, birthday) values (?, ?, ?, ?)",
-                user.getEmail(),
-                user.getLogin(),
-                user.getName(),
-                user.getBirthday()
-        );
+        checkName(user);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement pst = connection.prepareStatement(
+                    "insert into users (email, login, user_name, birthday) values (?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            pst.setString(1, user.getEmail());
+            pst.setString(2, user.getLogin());
+            pst.setString(3, user.getName());
+            pst.setDate(4, Date.valueOf(user.getBirthday()));
+            return pst;
+        }, keyHolder);
+        int id = keyHolder.getKey().intValue();
+        user.setId(id);
         return user;
     }
 
     @Override
     public User put(User user) {
+        checkName(user);
+        User foundUser = findById(user.getId());
         jdbcTemplate.update(
                 "update users set email = ?, login = ?, user_name = ?, birthday = ? where user_id = ?",
                 user.getEmail(),
@@ -84,6 +99,51 @@ public class UserDbStorage implements UserStorage {
         } else {
             log.info("Пользователь с идентификатором {} не найден.", id);
             throw new NotFoundException("Пользователь не найден");
+        }
+    }
+
+    public void addFriend(Integer userId, Integer friendId) {
+        User user = findById(userId);
+        User friend = findById(friendId);
+
+        Set<Integer> friends = user.getFriends();
+        friends.add(friendId);
+        user.setFriends(friends);
+
+        SqlRowSet userRows = jdbcTemplate.queryForRowSet(
+                "select * from friendship where (user1_id = ? and user2_id = ?) or (user1_id = ? and user2_id = ?)",
+                userId,
+                friendId,
+                friendId,
+                userId);
+        if (userRows.next()) {
+            friends = friend.getFriends();
+            friends.add(userId);
+            friend.setFriends(friends);
+
+            SqlRowSet friendshipStatus = jdbcTemplate.queryForRowSet("select * from friendship_status where status_name = ?", "Подтверждено");
+            jdbcTemplate.update(
+                    "update friendship set status_id = ? (where user1_id = ? and user2_id = ?) or (user1_id = ? and user2_id = ?)",
+                    friendshipStatus.getInt("status_id"),
+                    userId,
+                    friendId,
+                    friendId,
+                    userId
+            );
+        } else {
+            SqlRowSet friendshipStatus = jdbcTemplate.queryForRowSet("select * from friendship_status where status_name = ?", "Отправлен запрос");
+            jdbcTemplate.update(
+                    "insert into friendship (user1_id, user2_id, status_id) values (?, ?, ?)",
+                    userId,
+                    friendId,
+                    friendshipStatus.getInt("status_id")
+            );
+        }
+    }
+
+    private void checkName(User user) {
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
         }
     }
 }
